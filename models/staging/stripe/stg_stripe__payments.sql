701e5257f57ebf7fd3f2318dfaad40bd5b9973ed
Harry Waldeland <harrywaldeland@hotmail.com>
2026-04-20 09:01:08 +0000

    Changed from status to payment_status in stg_stripe__payments. Added fct_orders.sql

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -2,7 +2,7 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
-    status,
+    status as payment_status,
     amount,
     created as created_at,
     _batched_at
