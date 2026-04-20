c37396f3145ab9e276a7fb2ce9f746be53edd518
Harry Waldeland <harrywaldeland@hotmail.com>
2026-04-20 09:18:26 +0000

    changed column name from amount to payment_amount in stg_stripe__payments.sql

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,7 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status as payment_status,
-    amount,
+    amount as payment_amount,
     created as created_at,
     _batched_at
 from {{ source('stripe', 'payment') }}
