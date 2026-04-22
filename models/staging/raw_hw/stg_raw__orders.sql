308f3257b97c19d76c5f768a09a74c4f1d40e05f
Harry Waldeland <harrywaldeland@hotmail.com>
2026-04-22 12:23:14 +0000

    Changed status to order_status in order model. Added Descripton for order_status. Added som basic tests.

--- models/staging/raw_hw/stg_raw__orders.sql
+++ models/staging/raw_hw/stg_raw__orders.sql
@@ -2,6 +2,6 @@ select
         try_to_number(id) as order_id,
         try_to_number(user_id) as customer_id,
         order_date,
-        status,
+        status as order_status,
         try_to_number(order_total) as order_total
     from {{ source('raw_hw', 'orders') }}
