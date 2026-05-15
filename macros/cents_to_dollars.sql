83e2aa486d4f86e9cb0b21b376ef95efc30c0618
Harry Waldeland <harrywaldeland@hotmail.com>
2026-05-15 12:08:33 +0000

    Minor changes and also deletes due to Snowflake problems.

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,5 +1,5 @@
-{% macro cents_to_dollars(column_name) %}
-
-    {{ column_name }} / 100
+{% macro cents_to_dollars(column_name, decimals=2) %}
+    
+    ROUND( 1.0 * {{ column_name }} / 100, {{ decimals }})
 
 {% endmacro %}
